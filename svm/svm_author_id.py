c08df641c036d9a354334cda810ab53fbdab02b3
Azizur Rahman <Sarzil@Azizurs-MacBook-Pro.local>
2017-04-05 00:53:02 -0400

    full training set

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,8 +26,8 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 from sklearn.svm import SVC
 clf = SVC(kernel="rbf",C = 10000.0)
-features_train = features_train[:len(features_train)/100] 
-labels_train = labels_train[:len(labels_train)/100] 
+
+
 clf.fit(features_train,labels_train)
 pred = clf.predict(features_test)
 #########################################################
